7de32c1da027e36e7931b38d409e3296030010cb
Josh Cohn <joshcohn27@gmail.com>
2026-04-10 00:16:33 -0400

    esc to X

--- src/poetry/PoetryMain.tsx
+++ src/poetry/PoetryMain.tsx
@@ -440,7 +440,7 @@ export default function Poetry() {
                                 (e.currentTarget as HTMLButtonElement).style.background = "rgba(255,255,255,0.06)";
                             }}
                         >
-                            esc
+                            X
                         </button>
 
                         <h2 style={{
